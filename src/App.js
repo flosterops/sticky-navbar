53bba57b41560639caab5ae29747d84042a3bfa5
flosterops <deniska.veklich@gmail.com>
2021-10-06 21:31:57 +0300

    added component to the app, simulated api request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavBar } from "./components";
+import { useEffect, useState } from "react";
+import { Companies } from "./components/Companies";
+import { fetchCompanies } from "./requests/fetchCompanies";
 
-function App() {
+const App = () => {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      const data = await fetchCompanies();
+      setCompanies(data);
+    };
+
+    handleFetch();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Companies companies={companies} />
+    </>
   );
-}
+};
 
-export default App;
+export { App };
